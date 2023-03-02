e7f5bceb69e0f214211b517a8ea0326449a1ab45
Vadim_Niziyov <Vadim.Niziev@rt.ru>
2023-03-02 22:25:34 +0300

    2.16-2.17

---
+++ learnJs/2_Basic_JavaScript/2.17_arrow_functions/2.17_arrow_functions_theory.js
@@ -0,0 +1,14 @@
+/**
+ * Cинтаксис
+ */
+{
+    let func = (arg1, arg2, ...argN) => arg1;
+    let double = n => n * 2;
+    let sayHi = () => alert("Hello!");
+}
+{
+    //Является сокращенной версией от FE
+    let func = function(arg1, arg2, ...argN) {
+        return arg1;
+    }
+}
