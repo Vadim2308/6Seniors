d31764dc7e59c256e59001a20da3937c1358e9fa
Vadim Niziyov <st1mpro23@yandex.ru>
2023-08-27 14:33:48 +0300

    minor fix

--- MenthorsTasks/trueFalseObjectByKatana.js
+++ MenthorsTasks/trueFalseObjectByKatana.js
@@ -1,19 +1,19 @@
 /**
- * Реализовать объект, который при сравнении (obj > obj) с самим собой будет давать false/true/false/true/false (Katana)
+ * Реализовать объект, который при сравнении (obj > obj) с самим собой будет давать true/false/true/false (Katana)
  */
 
 const obj = {
     value: 0,
     [Symbol.toPrimitive]: function(hint) {
         if (hint === 'number') {
-            this.value++
+            this.value--
             return this.value % 4;
         }
         return this;
     }
 };
 
-console.log(obj > obj); // false     1 > 2 === false
-console.log(obj > obj); // true      3 > 0 === true
-console.log(obj > obj); // false
+console.log(obj > obj); // true     -1 > -2 === true
+console.log(obj > obj); // false      -3 > 0 === false
 console.log(obj > obj); // true
+console.log(obj > obj); // false
