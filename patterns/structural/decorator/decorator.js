12a653357977e3c6d7f0f3d30e77e9bcbeae0e88
Vadim Niziyov <st1mpro23@yandex.ru>
2023-12-21 23:45:35 +0300

    Добавлены паттеры декоратора и фасада

--- patterns/structural/decorator/decorator.js
+++ patterns/structural/decorator/decorator.js
@@ -1,21 +1,27 @@
-class Car {
+/**
+ *
+ * ПАТТЕРН ДЕКОРАТОР (обертка)
+ * Позволяет наделить обьект новыми возможностями не меняя первоначальный класс
+ * Принцип работы: декоратор помещает целевой обьект в обьект обертку, кот-й запускает базовое поведение обьекта,
+ * а затем добавляет/отнимает что-то свое.
+ *
+ */
 
+class Car {
 	constructor() {
 		this.price = 10000;
 		this.model = 'Car'
 	}
-  
+
 	getPrice() {
 		return this.price;
 	}
-  
 	getDescription() {
 		return this.model
 	}
 }
 
 class Tesla extends Car {
-
 	constructor() {
 		super();
 		this.price = 25000;
@@ -26,12 +32,10 @@ class Tesla extends Car {
 class Autopilot {
 	constructor(car) {
 		this.car = car;
-  	}
-
+}
 	getPrice() {
 		return this.car.getPrice() + 5000;
 	}
-  
 	getDescription() {
 		return `${this.car.getDescription()} with autopilot`;
 	}
@@ -45,8 +49,16 @@ class Parktronic {
 	getPrice() {
 		return this.car.getPrice() + 3000;
 	}
-  
 	getDescription() {
 		return `${this.car.getDescription()} with parktronic`;
 	}
 }
+
+/**
+ * Использование
+ */
+let tesla = new Tesla()
+tesla = new Autopilot(tesla)
+tesla = new Parktronic(tesla)
+
+console.log({price:tesla.getPrice(),description:tesla.getDescription()}) // {price: 33000, description: "Tesla with autopilot with parktronic"}
--- patterns/structural/facade/facade.js
+++ patterns/structural/facade/facade.js
@@ -1,48 +1,44 @@
-class Сonveyor {
+/**
+ * Задача паттера скрыть сложную логику за "Фасадом"
+ * Его задача объединить сложные структуру, и наружу отдавать понятный интерфейс взаимодействия.
+ * Если в коде встречаются большие и сложные объекты, то его можно смело заворачивать в класс фасада
+ */
 
+class Conveyor {
 	setBody() {
 		console.log('Body set!');
 	}
-
 	getEngine() {
-        console.log('Dismantle Engine!');
-    }
-
+       console.log('Dismantle Engine!');
+    }
 	setEngine() {
 		console.log('Engine set!');
 	}
-
 	setInterior() {
 		console.log('Exterior added!');
 	}
-
 	changeInterior() {
 		console.log('Update interior!');
 	}
-
 	setExterior() {
 		console.log('Added interior!');
 	}
-
 	setWheels() {
 		console.log('Wheels!');
 	}
-
 	addElectronics() {
 		console.log('Added electronics!');
 	}
-
 	paint() {
 		console.log('Car painted!');
 	}
 }
 
-class СonveyorFacade {
+class ConveyorFacade {
 	constructor(car) {
 		this.car = car;
 	}
-
-	assembleCar() {
+	assembleCar() { // У нас есть один удобный метод, реализвация которого спратяна за фасадом
 		this.car.setBody();
 		this.car.setEngine();
 		this.car.setInterior();
@@ -50,5 +46,21 @@ class СonveyorFacade {
 		this.car.setWheels();
 		this.car.addElectronics();
 		this.car.paint();
+		return this.car
 	}
 }
+
+/**
+ * Использование
+ */
+const conveyor = new ConveyorFacade(new Conveyor())
+const car = conveyor.assembleCar()
+/**
+ * Body set!
+ * Engine set!
+ * Exterior added!
+ * Added interior!
+ * Wheels!
+ * Added electronics!
+ * Car painted!
+ */
